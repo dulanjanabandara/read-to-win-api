5fa227cefacbab487f28347e802fb14f6035767a
Dulanjana Bandara <dulanjanauop@gmail.com>
2023-06-27 23:07:53 +0530

    Implemented global error handler for the api.

---
+++ controllers/errorController.js
@@ -0,0 +1,42 @@
+const AppError = require('../utils/AppError');
+
+const sendErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+};
+
+const sendErrorProd = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    const error = { ...err };
+    error.message = err.message;
+
+    sendErrorProd(error, req, res);
+  }
+};
