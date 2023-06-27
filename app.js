2931d8227680678c498cceb118e893c456420e2d
Dulanjana Bandara <dulanjanauop@gmail.com>
2023-06-28 00:23:43 +0530

    Book routes were mounted.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const AppError = require('./utils/appError');
+
+const bookRouter = require('./routes/bookRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use('/api/v1/books', bookRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
